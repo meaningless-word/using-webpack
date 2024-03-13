6918b2e191b0f611a03e9e243fb5180d4c898bd1
meaningless-word <grisly@mail.ru>
2024-03-13 20:29:18 +0700

    E12.4. JS-модули

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-document.write('Hello,  world! \n');
+//document.write('Hello,  world! \n');
+
+const mult = (x) => 2 * x;
+
+// можно импортнуть списком, некоторых с алиасом, чтоб не пересекались именами с функциями в этом модуле
+import { sum, mult as multiply} from './math';
+// можно так же импортнуть весь модуль под видом объекта, тогда к его функциям можно обращаться как к методам
+import * as math from './math';
+
+console.log(`вызов функции sum(2, 3) из модуля math вщзвращает результат: ${sum(2, 3)}`);
+console.log(`вызов функции mult(3) из текущего модулы возвращает: ${mult(3)}`);
+console.log(`а это вызов импортированной функции mult(3, 4) под алиасом multiply: ${multiply(3, 4)}`);
+console.log(`площадь круга с радиусом 3 = ${math.circleArea(3)}`);
---
+++ src/math.js
@@ -0,0 +1,7 @@
+// так пределяются константы, доступные для импорта из этого модуля
+export const PI = 3.14159265359
+
+// а так функции
+export const sum = (a, b) => a + b;
+export const mult = (a, b) => a * b;
+export const circleArea = (r) => PI * r**2;
